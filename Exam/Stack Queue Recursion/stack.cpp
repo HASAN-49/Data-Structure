f8b1636d258221be3cbdda13352f43d23cae092e
HASAN-49 <107288719+HASAN-49@users.noreply.github.com>
2023-08-10 19:58:37 +0600

    Push and Deletion operation on a stack

---
+++ Exam/Stack Queue Recursion/stack.cpp
@@ -0,0 +1,73 @@
+// push and deletion in stack
+#include<bits/stdc++.h>
+using namespace std;
+
+#define MAX 10
+int top = 0;
+
+void push(int stk[], int value) {
+    if(top == MAX) {
+        cout << "Stack Overflow!!!\n";
+        return;
+    }
+    stk[top] = value;
+    top += 1;
+    //return top;
+}
+
+void pop(int stk[]) {
+    if(top == 0) {
+        cout << "Stack Underflow!!!\n";
+        return;
+    }
+    int item = stk[top];
+    top--;
+}
+
+void display(int stk[]) {
+    if(top == 0) cout << "No elements...\n";
+    for(int i = 0; i < top; i++) {
+        cout << stk[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int stk[MAX];
+
+    push(stk, 1);
+    push(stk, 2);
+    push(stk, 1);
+    push(stk, 2);
+    push(stk, 1);
+    push(stk, 2);
+    push(stk, 1);
+    push(stk, 2);
+    push(stk, 1);
+    push(stk, 2);
+    push(stk, 1);
+    push(stk, 2);
+
+    cout << "Elements on stack after push : ";
+    display(stk);
+
+    pop(stk);
+    pop(stk);
+    pop(stk);
+    pop(stk);
+    pop(stk);
+    pop(stk);
+    pop(stk);
+    cout << "After some elements deleted : ";
+    display(stk);
+    pop(stk);
+    pop(stk);
+    pop(stk);
+    pop(stk);
+    pop(stk);
+
+    cout << "After some more deletion : ";
+    display(stk);
+
+    return 0;
+}
